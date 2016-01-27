4697208613ad4240d9717183e5461f81df7862c7
krujos <jkruck@pivotal.io>
2016-01-27 11:33:24 -0800

    Twirl around application.properties to application.yaml

--- src/main/java/com/homedepot/service/EmailerService.java
+++ src/main/java/com/homedepot/service/EmailerService.java
@@ -1,12 +1,24 @@
 package com.homedepot.service;
 
 import com.homedepot.entity.EmailDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailerService {
 
+    private MailSender sender;
+    private EmailGenerator generator;
+
+    @Autowired
+    public EmailerService(MailSender sender) {
+        this.sender = sender;
+        this.generator = new EmailGenerator();
+    }
+
     public boolean sendEmail(EmailDetails details) {
+        sender.send(generator.generateEmail(details));
         return true;
     }
 
--- src/test/java/com/homedepot/service/EmailerServiceTest.java
+++ src/test/java/com/homedepot/service/EmailerServiceTest.java
@@ -3,26 +3,45 @@ package com.homedepot.service;
 import com.homedepot.entity.EmailDetails;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class EmailerServiceTest{
 
     private EmailDetails details;
     private EmailerService emailService;
 
+    @Mock
+    MailSender sender;
+
     @Before
     public void setUp() {
+
+        MockitoAnnotations.initMocks(this);
+
         details = new EmailDetails();
         details.setAccessRequirementsRepo("https://github.com/foo/repo/file");
         details.setAppId("the-id");
         details.setDesiredVaultUrl("https://vault.io/secret/key");
         details.setSpaceGuid("the-guid");
 
-        emailService = new EmailerService();
+        emailService = new EmailerService(sender);
     }
     @Test
     public void itShouldSendAnEmailWithValidDetails() {
         assertTrue(emailService.sendEmail(details));
+    }
+
+    @Test
+    public void itShouldUseTheSenderToSendAMessage() {
+        emailService.sendEmail(details);
+        verify(sender, times(1)).send(any(SimpleMailMessage.class));
     }
 }
